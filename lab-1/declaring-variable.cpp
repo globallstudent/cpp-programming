9ffb61a2b624760a93f72c6a45c069d3b37092be
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2024-09-12 16:16:07 +0500

    New files

---
+++ lab-1/declaring-variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string var = "Hello";   // Added a semicolon here
+    int var1 = 1;
+    float var2 = 1.2;
+    double var3 = 3.14159265;
+    char var4 = 'a';
+    bool var5 = true;
+
+    // Corrected the usage of cout
+    cout << var << "\n " << var1 << "\n " << var2 << "\n " << var3 << "\n " << var4 << "\n " << var5 << endl;
+    return 0;
+}
+
