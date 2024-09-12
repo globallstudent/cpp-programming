d4beac6e5c50ce7074a5cadf7ff472769bfc8ebb
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2024-09-12 16:37:47 +0500

    New files

---
+++ lab-1/add-integers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "Enter a: ";
+    cin >> num1;
+
+    cout << "Enter b: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << sum << endl;
+
+    return 0;
+
+}
