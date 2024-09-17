1e9b0261649689aad7206de991fc67379d5a171a
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2024-09-17 20:54:21 +0500

    Exercise 9

--- exercises/chapter_2/09.cpp
+++ exercises/chapter_2/09.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+    int a, b, c, d;
+    char dummychar;
 
-    int fraction;
-    cout << "Enter fraction: ";
-    cin >> fraction;
+    // Getting the first fraction from the user
+    cout << "Enter first fraction (in the form a/b): ";
+    cin >> a >> dummychar >> b;
 
-    cout << fraction;
+    // Getting the second fraction from the user
+    cout << "Enter second fraction (in the form c/d): ";
+    cin >> c >> dummychar >> d;
+
+    // Calculating the numerator and denominator of the sum
+    int numerator = a * d + b * c;
+    int denominator = b * d;
+
+    // Displaying the result
+    cout << "Sum = " << numerator << "/" << denominator << endl;
 
     return 0;
-
 }
+
