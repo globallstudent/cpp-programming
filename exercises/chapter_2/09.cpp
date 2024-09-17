00f0a3327775a196274967a3ac5f3e855038ff23
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2024-09-17 20:50:12 +0500

    Exercise 9

---
+++ exercises/chapter_2/09.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int fraction;
+    cout << "Enter fraction: ";
+    cin >> fraction;
+
+    cout << fraction;
+
+    return 0;
+
+}
